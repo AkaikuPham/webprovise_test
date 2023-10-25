91738e21f84f07ee3201926783ae02180c40cd19
AkaikuPham <user.email=long2kpham@gmail.com>
2023-10-25 17:08:24 +0700

    [Feature] get company tree

---
+++ config/const.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Pre-defined Values
+|--------------------------------------------------------------------------
+ */
+
+return [
+    'api_company_url' => 'https://5f27781bf5d27e001612e057.mockapi.io/webprovise/companies',
+    'api_travel_url' => 'https://5f27781bf5d27e001612e057.mockapi.io/webprovise/travels'
+];
